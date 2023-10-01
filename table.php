a93cae21c2db9ac034c966c4fe3c1d169ae4a66d
hugosandsjo <hugosandsjo@gmail.com>
2023-10-01 11:17:34 +0200

    Table done

---
+++ table.php
@@ -0,0 +1,23 @@
+<?php require __DIR__ . '/data.php'; ?>
+
+<table>
+    <tr>
+        <th>Team</th>
+        <th>League</th>
+        <th>Last-time-champions</th>
+        <th>City</th>
+        <th>url</th>
+    </tr>
+
+    <?php foreach ($teams as $key => $team) : ?>
+
+        <tr>
+            <td> <?php echo count($team); ?> </td>
+            <td> <?php echo $team['league']; ?> </td>
+            <td> <?php echo $team['last-time-champions']; ?> </td>
+            <td> <?php echo $team['city']; ?> </td>
+            <td> <?php echo $team['url']; ?> </td>
+        </tr>
+
+    <?php endforeach; ?>
+</table>
